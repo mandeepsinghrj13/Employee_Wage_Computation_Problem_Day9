6b7311c399da4294ec38ef7d6c6d428300e22552
mandeep <singh.mr.1996@gmail.com>
2021-06-04 18:01:53 +0530

    uc1_check_employee_present_or_absent

--- EmployeeWageComputionProblem.sh
+++ EmployeeWageComputionProblem.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 echo "Employee Wage Computation Program Push On Master"
+
+isPresent=1
+randomNumber=$((RANDOM%2))
+if [ $randomNumber -eq 	$isPresent ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
